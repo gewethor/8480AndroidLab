4b115ce54fcd8da5ee9bc7fdab36537716a0815e
gewethor <gwethor@gmail.com>
2017-03-26 14:28:15 -0500

    Radio Group orientation changes dynamically

--- app/src/main/java/com/vogella/android/testapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/testapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -12,8 +14,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.Horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.Vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                }
+            }
+        });
+    }
     public void onClick (View view) {
         EditText input = (EditText) findViewById(R.id.main_input);
         String string = input.getText().toString();
