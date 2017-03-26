902b4aaa4ed6a1f708f8711b85ba70410ce833d7
gewethor <gwethor@gmail.com>
2017-03-26 17:45:34 -0500

    Creating a Temp Converter

--- app/src/main/java/com/vogella/android/testapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/testapp/MainActivity.java
@@ -3,26 +3,46 @@ package com.vogella.android.testapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private EditText text;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView view =        (TextView) findViewById(R.id.TextView02);
-        String s="";
-        for (int i=0; i < 500; i++) {
-            s += "vogella.com ";
-        }
-        view.setText(s);
-    }
-}
+        text = (EditText) findViewById(R.id.inputValue);
 
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                } else {
+                    text.setText(String
+                            .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+        }
+    }
+
+}
